5360894ff024b211c2a88d00f5b1669436be0e0b
Goossens Levi <Levi.Goossens@student.pxl.be>
2025-05-20 22:01:05 +0200

    MAUI-6

--- MauiAppLevi/RestService.cs
+++ MauiAppLevi/RestService.cs
@@ -9,5 +9,6 @@ namespace MauiAppLevi
     internal class RestService
     {
         HttpClient _client = new HttpClient();
+        private const string REST_URL = "https://h9z33q0b-7220.euw.devtunnels.ms/api/";
     }
 }
