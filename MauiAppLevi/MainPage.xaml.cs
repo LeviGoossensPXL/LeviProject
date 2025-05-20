7bd01239a41da279198cdc656cb544f13d77ab49
Goossens Levi <Levi.Goossens@student.pxl.be>
2025-05-20 22:04:43 +0200

    MAUI-05

--- MauiAppLevi/MainPage.xaml.cs
+++ MauiAppLevi/MainPage.xaml.cs
@@ -12,7 +12,7 @@ namespace MauiAppLevi
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnLoadBtnClick(object sender, EventArgs e)
         {
             count++;
             ListView.ItemsSource = new List<Article>
